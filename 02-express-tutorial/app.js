0f8eef41fdd024510df980dfd92b7c31b4d17aea
judeakinwale <judeakinwale@gmail.com>
2021-10-18 09:59:34 +0100

    Using middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,70 +1,33 @@
-const { response } = require('express')
 const express = require('express')
-const {products, people} = require('./data')
+const morgan = require('morgan')
+const logger = require('./logger')
+const authorize = require('./authorize')
 
 const app = express()
 
-// // get static files
-// // will also use the index.html in the public folder as the home page
-// app.use(express.static('./public'))
+// // only applies to paths starting with /api
+// app.use('/api', logger)
 
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
+
+// req => middleware => res
 app.get('/', (req, res) => {
-    res.send("<h1>Home Page</h1> <a href='/api/products'>products</a>")
+    res.send('Home')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
 })
 
 app.get('/api/products', (req, res) => {
-    const newProducts = products.map((products) => {    
-        const {id, name, image, price} = products
-        return {id, name, image, price}
-    })
-    res.json(newProducts)
+    res.send('Products')
 })
 
-app.get('/api/products/:productId', (req, res) => {
-    // console.log(req)
-    // console.log(req.params)
-    const {productId} = req.params
-    const singleProduct = products.find(
-        (product) => product.id === Number(productId)
-    )
-
-    if (!singleProduct) {
-        return res.status(404).send("Product does not exist")
-    }
-    return res.json(singleProduct)
-})
-
-app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
-    console.log(req.params)
-    res.send('Hello world')
-})
-
-app.get('/api/v1/query', (req, res) => {
-    console.log(req.query)
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if (search) {
-        sortedProducts = sortedProducts.filter((products) => {
-            return products.name.startsWith(search)
-        })
-    }
-
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-
-    // if there are no products matching the search
-    if (sortedProducts.length < 1) {
-        // return res.status(200).send('No products matched your search')
-        return res.status(200).json({
-            sucess: true,
-            data: [],   
-        })
-    } 
-
-    return res.status(200).json(sortedProducts)
-    // return res.send("hello world")
+app.get('/api/items', (req, res) => {
+    console.log(req.user)
+    res.send('Items')
 })
 
 app.listen(5000, () => {
